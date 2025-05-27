358492a4db66bfe51a8f866a37b924cf706542f1
Alan Telles <alantelles86@gmail.com>
2025-05-27 18:39:43 -0300

    feat: define tipo do conteudo

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ const (
 	ContentTypeText   = "text/plain; charset=utf-8"
 
 	AppLocationToken = "{{APP_LOCATION}}"
+
+	TypeCommand = "COMMAND"
+	TypeText    = "TEXT"
+	TypeImage   = "IMAGE"
+	TypeVideo   = "VIDEO"
+	TypeAudio   = "AUDIO"
+	TypeBinary  = "BINARY"
 )
 
 type flagsSetup struct {
@@ -28,7 +35,7 @@ type flagsSetup struct {
 type mediaProviderContent struct {
 	ProviderId int    `json:"providerId"`
 	Content    string `json:"content"`
-	IsBinary   bool   `json:"isBinary"`
+	Type       string `json:"type"`
 }
 
 type media struct {
@@ -47,7 +54,7 @@ type returnBody struct {
 var flagsUsed flagsSetup
 
 var provider1 = mediaProviderContent{
-	ProviderId: 1, Content: "", IsBinary: false,
+	ProviderId: 1, Content: "", Type: TypeText,
 }
 
 var port = 8080 // TODO: receive this by running argument
