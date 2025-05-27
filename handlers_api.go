c1b39641370f89726db81fa2cf00fb61a059c0dd
Alan Telles <alantelles86@gmail.com>
2025-05-27 20:42:30 -0300

    feat: possibilita o uso de diversos container de texto \o/

--- handlers_api.go
+++ handlers_api.go
@@ -21,14 +21,22 @@ func getSongsFolderList(c *gin.Context) {
 }
 
 func setMediaProviderContent(c *gin.Context) {
-	var newContent mediaProviderContent
+	providerId := c.Param("providerId")
+	var newContent ProviderData
 	if err := c.BindJSON(&newContent); err != nil {
 		return
 	}
-	provider1 = newContent
+	err := CopyIncomingProviderToExistent(providerId, newContent)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
 	response := returnBody{
-		Status:  http.StatusCreated,
-		Message: "New content setup to provider1",
+		Status:     http.StatusCreated,
+		Message:    "New content setup",
+		ProviderId: providerId,
+		Type:       newContent.Type,
+		ContentId:  newContent.ContentId,
 	}
 	CORS(c)
 	c.JSON(http.StatusCreated, response)
@@ -36,7 +44,9 @@ func setMediaProviderContent(c *gin.Context) {
 
 func getMediaProviderContent(c *gin.Context) {
 	CORS(c)
-	c.JSON(http.StatusOK, provider1)
+	providerId := c.Query("providerId")
+	provider := providers[providerId]
+	c.JSON(http.StatusOK, &provider)
 }
 
 func saveMedia(c *gin.Context) {
