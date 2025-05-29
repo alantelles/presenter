033ef961c38a61212799bcb3dec8ab8e97f53bd3
Alan Telles <alantelles86@gmail.com>
2025-05-29 00:35:22 -0300

    Revert "feat: possibilita o uso de diversos container de texto \o/"
    This reverts commit ec482fe6839bd06cf0d439fdcd4837fdc0715477.

--- handlers_api.go
+++ handlers_api.go
@@ -21,22 +21,14 @@ func getSongsFolderList(c *gin.Context) {
 }
 
 func setMediaProviderContent(c *gin.Context) {
-	providerId := c.Param("providerId")
-	var newContent ProviderData
+	var newContent mediaProviderContent
 	if err := c.BindJSON(&newContent); err != nil {
 		return
 	}
-	err := CopyIncomingProviderToExistent(providerId, newContent)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-		return
-	}
+	provider1 = newContent
 	response := returnBody{
-		Status:     http.StatusCreated,
-		Message:    "New content setup",
-		ProviderId: providerId,
-		Type:       newContent.Type,
-		ContentId:  newContent.ContentId,
+		Status:  http.StatusCreated,
+		Message: "New content setup to provider1",
 	}
 	CORS(c)
 	c.JSON(http.StatusCreated, response)
@@ -44,9 +36,7 @@ func setMediaProviderContent(c *gin.Context) {
 
 func getMediaProviderContent(c *gin.Context) {
 	CORS(c)
-	providerId := c.Query("providerId")
-	provider := providers[providerId]
-	c.JSON(http.StatusOK, &provider)
+	c.JSON(http.StatusOK, provider1)
 }
 
 func saveMedia(c *gin.Context) {
