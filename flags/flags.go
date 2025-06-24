6a90f6f2c07c2bbc00eb7a6ff41629965772ea06
Alan Telles <alantelles86@gmail.com>
2025-06-24 00:08:40 -0300

    feat: possibilita passar token biblia digital

--- flags/flags.go
+++ flags/flags.go
@@ -1,21 +1,41 @@
 package flags
 
-import "flag"
+import (
+	"flag"
+)
 
 type FlagsSetup struct {
-	Location string
+	Location           string
+	TokenBibliaDigital string
 }
 
 var FlagsUsed FlagsSetup
 
 func ProcessFlags() {
-	addrDescription := "Endereço para acessar o serviço. Será utilizado nas requisições internas das páginas e para acesso às telas de operação. " +
-		"Inclua o endereço todo, incluindo o esquema (http/https) e porta, se houver necessidade. Isto não muda a porta de execução da aplicação"
-	addrPtr := flag.String("endereco", "", addrDescription)
+	locationPtr := GetLocationFlag()
+	tokenBibliaDigitalTokenPtr := GetTokenBibliaDigitalTokenFlag()
 	flag.Parse()
-	FlagsUsed.Location = *addrPtr
+	FlagsUsed.Location = *locationPtr
+	FlagsUsed.TokenBibliaDigital = *tokenBibliaDigitalTokenPtr
+}
+
+func GetLocationFlag() *string {
+	description := "Endereço para acessar o serviço. Será utilizado nas requisições internas das páginas e para acesso às telas de operação. " +
+		"Inclua o endereço todo, incluindo o esquema (http/https) e porta, se houver necessidade. Isto não muda a porta de execução da aplicação"
+	return flag.String("endereco", "", description)
+}
+
+func GetTokenBibliaDigitalTokenFlag() *string {
+	description := "Token para acessar a API d'A Bíblia Digital (abibliadigital.com.br), " +
+		"o provedor de textos bíblicos utilizado pelo aplicativo. " +
+		"Se não for informado, o aplicativo não conseguirá acessar os textos bíblicos"
+	return flag.String("tokenBibliaDigital", "", description)
 }
 
 func GetLocation() string {
 	return FlagsUsed.Location
+}
+
+func GetTokenBibliaDigital() string {
+	return FlagsUsed.TokenBibliaDigital
 }
